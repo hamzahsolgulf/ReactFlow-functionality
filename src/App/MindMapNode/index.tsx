7026f5b9dae264941dc52adae12f631903c0e69a
Hamzah Imran <hamzahimran102@gmail.com>
2024-08-18 02:30:09 -0400

    can make new nodes now

--- src/App/MindMapNode/index.tsx
+++ src/App/MindMapNode/index.tsx
@@ -1,45 +1,47 @@
-/*
-    NodeData:
-        NodeData is a TypeScript type definition that specifies the structure of 
-        the data object expected by a node in your application. In this case, 
-        NodeData has a single property:
+import { useLayoutEffect, useEffect, useRef } from 'react';
+import { Handle, NodeProps, Position } from 'reactflow';
 
-        label: A string that represents some label or identifier for the node.
-    
-    Data Object:
-        A data object is a structured way to store and pass data within your 
-        application. In the context of the provided code, the data object adheres
-        to the NodeData type and is used to provide information to a node component.
-*/
+import useStore from '../store';
 
-import { Handle, NodeProps, Position } from "@xyflow/react";
+//import DragIcon from './DragIcon';
 
 export type NodeData = {
-  data: {
-    [key: string]: string;
-  };
-  id: string;
-  position: { x: number; y: number }
   label: string;
-  width: number;
-  height: number;
 };
 
 function MindMapNode({ id, data }: NodeProps<NodeData>) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const updateNodeLabel = useStore((state) => state.updateNodeLabel);
+
+  useEffect(() => {
+    setTimeout(() => {
+      inputRef.current?.focus({ preventScroll: true });
+    }, 1);
+  }, []);
+
+  useLayoutEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.style.width = `${data.label.length * 8}px`;
+    }
+  }, [data.label.length]);
+
   return (
     <>
-      <input title="reactMindMap node" defaultValue={data.label} />
+      <div className="inputWrapper">
+        
+        <input
+        title="reactMindMap node"
+          value={data.label}
+          onChange={(evt) => updateNodeLabel(id, evt.target.value)}
+          className="input"
+          ref={inputRef}
+        />
+      </div>
 
-      <Handle type="target" position={Position.Top} /> 
-      <Handle type="source" position={Position.Bottom} /> 
+      <Handle type="target" position={Position.Top} />
+      <Handle type="source" position={Position.Top} />
     </>
   );
 }
 
 export default MindMapNode;
-
-
-/*
-<Handle type="target" position={Position.Top} /> //the target handle is placed at the top of the node and This means other nodes can connect to this node via this handle.
-      <Handle type="source" position={Position.Bottom} /> //the source handle is placed at the bottom of the node and this means it can connect to other nodes using this example
-*/
--- src/App/store.ts
+++ src/App/store.ts
@@ -7,41 +7,76 @@ import {
     OnEdgesChange,
     applyNodeChanges,
     applyEdgeChanges,
-} from '@xyflow/react'
-
-import { createWithEqualityFn } from 'zustand/traditional'; 
-//zustand is a small state-management library
-/*The createWithEqualityFn function creates a store (a place where state is managed) 
-with a custom equality function, which helps optimize re-renders by comparing 
-the previous and next states.*/
-
-export type RFState = {
-    nodes: Node[]; //Node is a TypeScript type definition that specifies the structure of a node in your application.
+    XYPosition,
+  } from 'reactflow';
+  import { create } from 'zustand';
+  import { nanoid } from 'nanoid/non-secure';
+  
+  import { NodeData } from './MindMapNode';
+  
+  export type RFState = {
+    nodes: Node<NodeData>[];
     edges: Edge[];
-    onNodesChange: OnNodesChange; //OnNodesChange is a TypeScript type definition that specifies the structure of the onNodesChange function.
+    onNodesChange: OnNodesChange;
     onEdgesChange: OnEdgesChange;
-};
-
-const useStore = createWithEqualityFn<RFState>((set, get) => ({
+    updateNodeLabel: (nodeId: string, label: string) => void;
+    addChildNode: (parentNode: Node, position: XYPosition) => void;
+  };
+  
+  const useStore = create<RFState>((set, get) => ({
     nodes: [
-        {
-            id: 'root',
-            type:'mindmap',
-            data: { label: 'React Flow Mind Map' },
-            position: { x: 0, y: 0 },
-        },
+      {
+        id: 'root',
+        type: 'mindmap',
+        data: { label: 'React Flow Mind Map' },
+        position: { x: 0, y: 0 },
+        dragHandle: '.dragHandle',
+      },
     ],
     edges: [],
     onNodesChange: (changes: NodeChange[]) => {
-        set({
-            nodes: applyNodeChanges(changes, get().nodes),
-        });
+      set({
+        nodes: applyNodeChanges(changes, get().nodes),
+      });
     },
     onEdgesChange: (changes: EdgeChange[]) => {
-        set({
-            edges: applyEdgeChanges(changes, get().edges),
-        });
+      set({
+        edges: applyEdgeChanges(changes, get().edges),
+      });
     },
-}));
-
-export default useStore;
+    updateNodeLabel: (nodeId: string, label: string) => {
+      set({
+        nodes: get().nodes.map((node) => {
+          if (node.id === nodeId) {
+            // it's important to create a new object here, to inform React Flow about the changes
+            node.data = { ...node.data, label };
+          }
+  
+          return node;
+        }),
+      });
+    },
+    addChildNode: (parentNode: Node, position: XYPosition) => {
+      const newNode = {
+        id: nanoid(),
+        type: 'mindmap',
+        data: { label: 'New Node' },
+        position,
+        dragHandle: '.dragHandle',
+        parentNode: parentNode.id,
+      };
+  
+      const newEdge = {
+        id: nanoid(),
+        source: parentNode.id,
+        target: newNode.id,
+      };
+  
+      set({
+        nodes: [...get().nodes, newNode],
+        edges: [...get().edges, newEdge],
+      });
+    },
+  }));
+  
+  export default useStore;
